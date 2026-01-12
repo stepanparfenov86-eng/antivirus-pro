46b0b5ad151818c2242807d1cf6eee3b4f6f7dff
stepanparfenov86-eng <stepanparfenov86@gmail.com>
2026-01-12 13:51:13 +0300

    Update antivirus_pro.py

--- antivirus_pro.py
+++ antivirus_pro.py
@@ -1,7 +1,6 @@
 antivirus-pro/
-│
-├── antivirus_pro.py
-├── requirements.txt
-└── .github/
-    └── workflows/
-        └── build.yml
+antivirus_pro.py
+requirements.txt
+.github/
+workflows/
+build.yml
